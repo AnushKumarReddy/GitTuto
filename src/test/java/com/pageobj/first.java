e304f4ed020dc86143642d5abb3492c130ee8452
AnushKumarReddy <81006278+AnushKumarReddy@users.noreply.github.com>
2021-04-12 17:13:21 +0530

    some changes happened

--- src/test/java/com/pageobj/first.java
+++ src/test/java/com/pageobj/first.java
@@ -4,5 +4,9 @@ public class first {
 public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("good morning");
+	
+	System.out.println("hello world");
+	System.out.println("good morning");
+	
 }
 }
